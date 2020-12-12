3d0c60863e37d39cd7195202ea220612e5197156
David Carpenter <davidcarpenter510@gmail.com>
2020-12-11 22:11:17 -0700

    Working on updating esports component

--- src/components/esports.js
+++ src/components/esports.js
@@ -10,10 +10,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { blueGrey } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    backgroundColor: blueGrey,
   },
   media: {
     height: 140,
@@ -27,27 +29,21 @@ export default function Esports() {
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+          component='img'
+          alt='TeamLogo'
+          image={''}
+          title={''}
+          />
+          <CardContent>
+          <Typography gutterBottom variant='h5' component='h2'>
+            {''}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+          </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button>
+              Learn More
+            </Button>
       </CardActions>
     </Card>
   );
